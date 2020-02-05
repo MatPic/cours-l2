1c137a6f3999511041ea177fc81c10acc8d4cdbe
Ubuntu <ubuntu@ip-172-31-88-92.ec2.internal>
2020-02-05 07:24:22 +0000

    Test Button et Timer

--- button/src/components/button/Button.js
+++ button/src/components/button/Button.js
@@ -5,22 +5,22 @@ class Button extends React.Component {
 
   constructor(props) {
     super(props);
-    // ...
+    this.state = { toggled: false }
+    this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick() {
-    // ...
+    this.setState( { toggled: ! this.state.toggled })
   }
 
   render() {
     return (
       <button
+         className={ this.state.toggled ? "toggled" : "untoggled" }
          data-testid="button"
-         { /*
-            A remplir
-         */ }
+         onClick= { this.handleClick }
       >
-      {this.props.children ? this.props.children : "Add text!"}
+      {this.props.children ? this.props.children : "Click Me ! (Epico)"}
       </button>);
   }
 
--- timer/src/components/timer/Timer.js
+++ timer/src/components/timer/Timer.js
@@ -4,23 +4,37 @@ class Timer extends React.Component {
 
   constructor(props) {
     super(props);
-    // ...
+    this.state = { seconds: 0 }
+  }
+  
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
   }
 
   componentDidMount() {
-    // ...
+    this.interval = setInterval(() => this.tick(), 1000);
   }
 
   componentWillUnmount() {
-    // ...
+    clearInterval(this.interval);
   }
   
   render () {
-    return (
-      <div data-testid="timer">
-        {this.state.counter}
-      </div>
-    );
+    if (this.state.seconds <= 60) {
+      return (
+        <div data-testid="timer">
+          {this.state.seconds} secondes
+        </div>
+      );
+    } else {
+      return (
+        <div data-testid="timer">
+          1min et {this.state.seconds-59} secondes
+        </div>
+      );
+    }
   }
 }
 
